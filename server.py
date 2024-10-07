6b48b5ae586c21d308ee22b0d265ff37b9975a90
Arnie <u22203827@utp.edu.pe>
2024-10-07 00:44:43 -0500

    Server

--- server.py
+++ server.py
@@ -10,14 +10,7 @@ class FactorialServer:
             return n * self.factorial(n - 1)
 
 def start_server():
-    # Captura el puerto proporcionado por Render
-    port = os.getenv("PORT", "5000")  # Usa 5000 como valor predeterminado
-    try:
-        port = int(port)  # Intenta convertir a entero
-    except ValueError:
-        print(f"Valor no válido para el puerto: {port}. Usando 5000 como valor predeterminado.")
-        port = 5000
-
+    port = int(os.getenv("PORT", "9090"))
     daemon = Pyro4.Daemon(host="0.0.0.0", port=port)
 
     # Registra el servidor de nombres
@@ -25,7 +18,7 @@ def start_server():
     uri = daemon.register(FactorialServer)
     ns.register("factorial.server", uri)
 
-    print(f"Servidor factorial disponible en {uri}")  # Esta línea imprime la URI
+    print(f"Servidor factorial disponible en {uri}")
 
     # Mantiene el servidor corriendo
     daemon.requestLoop()
