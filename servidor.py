39a2a9bbed853340a52a23c08cb7d7a5a07cfd2c
Arnie <u22203827@utp.edu.pe>
2024-10-06 22:41:10 -0500

    Server

--- servidor.py
+++ servidor.py
@@ -1,4 +1,6 @@
 import Pyro4
+import Pyro4.naming
+
 
 @Pyro4.expose
 class FactorialCalculator:
@@ -10,13 +12,24 @@ class FactorialCalculator:
         else:
             return n * self.factorial(n - 1)
 
+
 def start_server():
-    Pyro4.Daemon.serveSimple(
-        {
-            FactorialCalculator: "factorial.calculator"
-        },
-        ns=True
-    )
+    # Iniciar el Nameserver
+    Pyro4.naming.startNS()
+
+    # Crear el daemon y registrar el objeto
+    daemon = Pyro4.Daemon()
+    uri = daemon.register(FactorialCalculator)
+
+    # Registrar el objeto en el Nameserver
+    ns = Pyro4.locateNS()
+    ns.register("factorial.calculator", uri)
+
+    print("El servidor está corriendo. Accede a través de 'factorial.calculator'")
+
+    # Ejecutar el servidor
+    daemon.requestLoop()
+
 
 if __name__ == "__main__":
     start_server()
